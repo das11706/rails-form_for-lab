afc8df461edea2c23572efecc11dd774f72f8b93
Daniel Suarez <suarez.daniel101@gmail.com>
2022-09-09 17:06:09 -0400

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,7 +1,12 @@
 class SchoolClassesController < ApplicationController
+
+  def new 
+
+  end
  
-def create
-  @school_class = SchoolClass.create(title: params[:school_class][:title], room_number: params[:school_class][:room_number])
-  redirect_to 
-end
+  def create
+    @school_class = SchoolClass.create(title: params[:school_class][:title], room_number: params[:school_class][:room_number])
+    redirect_to school_class_path(@school_class)
+  end
+
 end

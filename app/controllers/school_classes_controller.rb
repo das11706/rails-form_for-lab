62d3686050e17f0742e3db4e348a3244fc29adcc
Daniel Suarez <suarez.daniel101@gmail.com>
2022-09-12 18:03:20 -0400

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,12 +1,22 @@
 class SchoolClassesController < ApplicationController
 
-  def new 
+  def index
+    @school_classes = SchoolClass.all
+  end
 
+  def show
+    @school_class = SchoolClass.find(params[:id])
+  end
+
+  def new 
+    @school_class = SchoolClass.new
   end
  
   def create
-    @school_class = SchoolClass.create(title: params[:school_class][:title], room_number: params[:school_class][:room_number])
+    @school_class = SchoolClass.new(params.require(:school_class).permit(:title, :room_number))
+    @school_class.save
     redirect_to school_class_path(@school_class)
   end
-
 end
+
+ 
